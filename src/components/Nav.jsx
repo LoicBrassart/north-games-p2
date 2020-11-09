e4131d3f556a2690dcdbb71572e3ca1f8c1e776c
LoicBrassart <loic.brassart@gmail.com>
2020-11-09 15:17:41 +0100

    Add basic Nav+Routing

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,29 +1,35 @@
+import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 const SNav = styled.nav`
-  // Layout Grid
   grid-area: nav;
 
-  // Flex
   display: flex;
   flex-flow: column nowrap;
   justify-content: space-between;
+`;
 
-  // Children
-  li {
+const SNavLink = styled(NavLink)`
+  display: flex;
+
+  margin: 0.3em;
+  opacity: 0.5;
+
+  img {
+    height: 1.5em;
+    width: 1.5em;
+  }
+  p {
     display: flex;
-    img {
-      height: 1.5em;
-      width: 1.5em;
-    }
-    p {
-      // Flex
-      display: flex;
-      justify-content: center;
-      align-items: center;
+    justify-content: center;
+    align-items: center;
+    padding: 0.3em;
 
-      text-transform: uppercase;
-    }
+    text-transform: uppercase;
+  }
+
+  &:hover {
+    opacity: 1;
   }
 `;
 
@@ -32,25 +38,74 @@ export default function Nav() {
     <SNav>
       <ul>
         <li>
-          <img src="/assets/img/icons/home.svg" alt="Home" />
-          <p>Home</p>
+          <SNavLink to="/" activeClassName="active">
+            <img src="/assets/img/icons/home.svg" alt="Home" />
+            <p>Home</p>
+          </SNavLink>
+        </li>
+        <li>
+          <SNavLink to="/" activeClassName="active">
+            <img src="/assets/img/icons/user.svg" alt="Profile" />
+            <p>Profile</p>
+          </SNavLink>
         </li>
         <li>
           {" "}
-          <img src="/assets/img/icons/crowdfunding.svg" alt="Profile" />
-          <p>Profile</p>
+          <SNavLink to="/" activeClassName="active">
+            <img src="/assets/img/icons/messages.svg" alt="Profile" />
+            <p>Messages</p>
+          </SNavLink>
         </li>
-        <li>Messages</li>
-        <li>News</li>
-        <li>Events</li>
-        <li>Games</li>
-        <li>Groups</li>
-        <li>Games</li>
-        <li>Market Place</li>
+        <li>
+          <SNavLink to="/" activeClassName="active">
+            <img src="/assets/img/icons/news.svg" alt="Profile" />
+            <p>News</p>
+          </SNavLink>
+        </li>
+        <li>
+          <SNavLink to="/" activeClassName="active">
+            <img src="/assets/img/icons/events.svg" alt="Profile" />
+            <p>Events</p>
+          </SNavLink>
+        </li>
+        <li>
+          <SNavLink to="/" activeClassName="active">
+            <img src="/assets/img/icons/parties.svg" alt="Profile" />
+            <p>Parties</p>
+          </SNavLink>
+        </li>
+        <li>
+          <SNavLink to="/" activeClassName="active">
+            <img src="/assets/img/icons/groups.svg" alt="Profile" />
+            <p>Groups</p>
+          </SNavLink>
+        </li>
+        <li>
+          <SNavLink to="/" activeClassName="active">
+            <img src="/assets/img/icons/library.svg" alt="Profile" />
+            <p>Library</p>
+          </SNavLink>
+        </li>
+        <li>
+          <SNavLink to="/" activeClassName="active">
+            <img src="/assets/img/icons/market.svg" alt="Profile" />
+            <p>Market Place</p>
+          </SNavLink>
+        </li>
       </ul>
       <ul>
-        <li>Settings</li>
-        <li>FAQ</li>
+        <li>
+          <SNavLink to="/" activeClassName="active">
+            <img src="/assets/img/icons/settings.svg" alt="Profile" />
+            <p>Settings</p>
+          </SNavLink>
+        </li>
+        <li>
+          <SNavLink to="/" activeClassName="active">
+            <img src="/assets/img/icons/FAQ.svg" alt="Profile" />
+            <p>FAQ</p>
+          </SNavLink>
+        </li>
       </ul>
     </SNav>
   );
